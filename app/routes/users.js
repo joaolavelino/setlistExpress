979a07c4cce52a49b69d7898c220dee5dd6454c2
joaolavelino <joaolavelino@gmail.com>
2022-05-13 16:59:10 -0300

    5 - deploy troublehooting 3

--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var router = express.Router();
-const bcrypt = require("bcrypt");
+// const bcrypt = require("bcrypt");
 require("dotenv").config();
 const jwt = require("jsonwebtoken");
 const secret = process.env.JWT_TOKEN;
@@ -11,8 +11,8 @@ const User = require("../models/user.js");
 //USER REGISTER
 router.post("/register", async (req, res) => {
   const { name, email, password } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = new User({ name: name, email: email, password: hashedPassword });
+  // const hashedPassword = await bcrypt.hash(password, 10);
+  const user = new User({ name: name, email: email, password: password });
   try {
     await user.save();
     res.status(200).json(user);
@@ -27,7 +27,8 @@ router.post("/login", async (req, res) => {
     let user = await User.findOne({ email });
     if (!user) res.status(401).json({ error: "Invalid email or password" });
     else {
-      if (await bcrypt.compare(password, user.password)) {
+      // if (await bcrypt.compare(password, user.password)) {
+      if (password === user.password) {
         const token = jwt.sign({ email }, secret, { expiresIn: "10d" });
         res.json({ user: user, token: token });
       } else {
@@ -46,7 +47,8 @@ router.put("/edit/info", withAuth, async (req, res) => {
   const { name, email, password } = req.body;
   const update = { name: name, email: email };
   try {
-    if (await bcrypt.compare(password, req.user.password)) {
+    // if (await bcrypt.compare(password, req.user.password)) {
+    if (password === req.user.password) {
       console.log("password Ok");
       let user = await User.findByIdAndUpdate(req.user._id, update, {
         upsert: false,
@@ -64,10 +66,11 @@ router.put("/edit/info", withAuth, async (req, res) => {
 //USER PASSWORD EDIT
 router.put("/edit/password", withAuth, async (req, res) => {
   const { password, newPassword } = req.body;
-  const newHashedPassword = await bcrypt.hash(newPassword, 10);
-  const update = { password: newHashedPassword };
+  // const newHashedPassword = await bcrypt.hash(newPassword, 10);
+  const update = { password: newPassword };
   try {
-    if (await bcrypt.compare(password, req.user.password)) {
+    // if (await bcrypt.compare(password, req.user.password)) {
+    if (password === req.user.password) {
       let user = await User.findByIdAndUpdate(req.user._id, update, {
         upsert: false,
         new: true,
