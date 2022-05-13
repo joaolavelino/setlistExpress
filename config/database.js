3f6e3dc74c8759cd16e788f0e9b725f08c96c8cd
joaolavelino <joaolavelino@gmail.com>
2022-05-13 16:20:57 -0300

    5 - Deploy Setup

--- config/database.js
+++ config/database.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
+require("dotenv").config();
+const MONGO_URL = process.env.MONGO_URL;
 
 mongoose
-  .connect("mongodb://localhost/setlistExpress", {
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
